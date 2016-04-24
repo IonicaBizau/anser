529806d831e41cab41d5304685cf348410e266ab
Ionică Bizău <bizauionica@gmail.com>
2016-04-24 12:01:45 +0300

    :memo:

--- example/index.js
+++ example/index.js
@@ -7,6 +7,9 @@ let txt  = "\u001b[38;5;196mHello\u001b[39m \u001b[48;5;226mWorld\u001b[49m";
 console.log(Anser.ansiToHtml(txt));
 // <span style="color:rgb(255, 0, 0)">Hello</span> <span style="background-color:rgb(255, 255, 0)">World</span>
 
+console.log(Anser.ansiToHtml(txt, { use_classes: true }));
+// <span class="ansi-palette-196-fg">Hello</span> <span class="ansi-palette-226-bg">World</span>
+
 console.log(Anser.ansiToJson(txt));
 // [ { content: '',
 //     fg: null,
